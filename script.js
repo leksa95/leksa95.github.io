98a1d9a3b32e4bb57327d0642a8ce6c6d2d0abef
Leksa <oleksandra.shvets@yalantis.com>
2019-12-01 17:50:01 +0200

    fix animation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// $( document ).ready(function() {
+$( document ).ready(function() {
     $('.burgerBtn').click(function(e) {
         e.preventDefault();
         $('.navigation').toggleClass('hidden')
@@ -29,93 +29,11 @@
             // init slides containers
 
             setupHomeSection();
-
-        }
-        if ($(window).width() >= 768) {
             setupAdvantagesSection();
             setupActivitySection();
             setupProjectSection();
             setupContactSection();
             setupBlogSection();
-
-            function setupAdvantagesSection() {
-                var addScene = new ScrollMagic.Scene({
-                    triggerElement: $advantages[0],
-                    triggerHook: 0.6,
-                    reverse: true
-                })
-                    .setTween(
-                        new TimelineMax()
-                            .from($advantages.find('.subTitle '), 0.7, {opacity: 0, xPercent: -60})
-                            .staggerFrom($advantages.find('.suggestion-item > *'), 0.5, {
-                                opacity: 0,
-                                scale: 0
-                            }, 0.3, '-=0.3')
-                    )
-                    .addTo(controller)
-            }
-
-            function setupActivitySection() {
-                var activityScene = new ScrollMagic.Scene({
-                    triggerElement: $activity[0],
-                    triggerHook: 0.4,
-                    offset: -150,
-                    reverse: true
-                })
-                    .setTween(
-                        new TimelineMax()
-                            .from($activity.find('.subTitle '), 0.7, {opacity: 0, xPercent: 60})
-                            .staggerFrom($activity.find('.actionList .row > *'), 1, {opacity: 0, xPercent: -5}, 0.3)
-                    )
-                    .addTo(controller)
-            }
-
-            function setupProjectSection() {
-                var projectScene = new ScrollMagic.Scene({
-                    triggerElement: $projects[0],
-                    triggerHook: 0.5,
-                    // offset: -150,
-                    reverse: true
-                })
-                    .setTween(
-                        new TimelineMax()
-                            .from($projects.find('.subTitle '), 0.7, {opacity: 0, xPercent: -60})
-                            .staggerFrom($projects.find('.projectList .row > *'), 1, {opacity: 0, xPercent: -5}, 0.3)
-                            .from($projects.find('.toProject '), 0.5, {opacity: 0, xPercent: -5})
-                            .from($projects.find('.quote '), 0.5, {opacity: 0, scale: 0.5}, '+=0.3')
-                    )
-                    .addTo(controller)
-            }
-
-            function setupContactSection() {
-                var contactScene = new ScrollMagic.Scene({
-                    triggerElement: $contact[0],
-                    triggerHook: 0.5,
-                    // offset: -150,
-                    reverse: true
-                })
-                    .setTween(
-                        new TimelineMax()
-                            .from($contact.find('.titleBlock '), 0.7, {height: 0, padding: 0})
-                            .from($contact.find('.formContent '), 0.5, {opacity: 0})
-                            .from($contact.find('input[type="submit"] '), 0.5, {opacity: 0, scale: 0.5}, '+=0.3')
-                    )
-                    .addTo(controller)
-            }
-
-            function setupBlogSection() {
-                var blogScene = new ScrollMagic.Scene({
-                    triggerElement: $blog[0],
-                    triggerHook: 0.5,
-                    reverse: true
-                })
-                    .setTween(
-                        new TimelineMax()
-                            .from($blog.find('.blogTitle '), 0.7, {opacity: 0, xPercent: -20})
-                            .staggerFrom($blog.find('.blogList-wrapper > *'), 1, {opacity: 0, xPercent: -5}, 0.3)
-                    )
-                    .addTo(controller)
-            }
         }
         function setupHomeSection() {
             var homeScene = new ScrollMagic.Scene({
@@ -136,6 +54,78 @@
                 )
                 .addTo(controller)
         }
+        function setupAdvantagesSection() {
+            var addScene = new ScrollMagic.Scene({
+                triggerElement: $advantages[0],
+                triggerHook: 0.6,
+                reverse: true
+            })
+                .setTween(
+                    new TimelineMax()
+                        .from($advantages.find('.subTitle '), 0.7, { opacity: 0, xPercent: -60})
+                        .staggerFrom($advantages.find('.suggestion-item > *'), 0.5, {opacity: 0, scale: 0}, 0.3, '-=0.3')
+                )
+                .addTo(controller)
+        }
+        function setupActivitySection() {
+            var activityScene = new ScrollMagic.Scene({
+                triggerElement: $activity[0],
+                triggerHook: 0.4,
+                offset: -150,
+                reverse: true
+            })
+                .setTween(
+                    new TimelineMax()
+                        .from($activity.find('.subTitle '), 0.7, { opacity: 0, xPercent: 60})
+                        .staggerFrom($activity.find('.actionList .row > *'), 1, {opacity: 0, xPercent: -5}, 0.3)
+                )
+                .addTo(controller)
+        }
+        function setupProjectSection() {
+            var projectScene = new ScrollMagic.Scene({
+                triggerElement: $projects[0],
+                triggerHook: 0.5,
+                // offset: -150,
+                reverse: true
+            })
+                .setTween(
+                    new TimelineMax()
+                        .from($projects.find('.subTitle '), 0.7, { opacity: 0, xPercent: -60})
+                        .staggerFrom($projects.find('.projectList .row > *'), 1, {opacity: 0, xPercent: -5}, 0.3)
+                        .from($projects.find('.toProject '), 0.5, { opacity: 0, xPercent: -5})
+                        .from($projects.find('.quote '), 0.5,  { opacity: 0, scale: 0.5}, '+=0.3')
+                )
+                .addTo(controller)
+        }
+        function setupContactSection() {
+            var contactScene = new ScrollMagic.Scene({
+                triggerElement: $contact[0],
+                triggerHook: 0.5,
+                // offset: -150,
+                reverse: true
+            })
+                .setTween(
+                    new TimelineMax()
+                        .from($contact.find('.titleBlock '), 0.7, { height: 0, padding: 0})
+                        .from($contact.find('.formContent '), 0.5, { opacity: 0})
+                        .from($contact.find('input[type="submit"] '), 0.5,  { opacity: 0, scale: 0.5}, '+=0.3')
+                )
+                .addTo(controller)
+        }
+        function setupBlogSection() {
+            var blogScene = new ScrollMagic.Scene({
+                triggerElement: $blog[0],
+                triggerHook: 0.5,
+                reverse: true
+            })
+                .setTween(
+                    new TimelineMax()
+                        .from($blog.find('.blogTitle '), 0.7, { opacity: 0, xPercent: -20})
+                        .staggerFrom($blog.find('.blogList-wrapper > *'), 1, {opacity: 0, xPercent: -5}, 0.3)
+                )
+                .addTo(controller)
+        }
+
 
         function getBreakpoint() {
             if (width >= 1024) return BREAK_POINTS.DESKTOP;
@@ -143,7 +133,7 @@
             if (width >= 320 && width < 768) return BREAK_POINTS.MOBILE;
         }
     });
-// });
+});
 
 
 
